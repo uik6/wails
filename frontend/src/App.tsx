fa803b254b3652b09de56abe10c7bfb6438edee8
uik6 <53418637+uik6@users.noreply.github.com>
2024-11-06 16:11:44 +0800

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -34,7 +34,7 @@ const App: React.FC = () => {
         </Header>
         <Content
           style={{
-            margin: "24px 16px",
+            margin: "16px 16px",
             padding: 24,
             minHeight: 280,
             background: colorBgContainer,
