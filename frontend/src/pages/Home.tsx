682f272f6b9a448cefe1a5a14a3f82cf37d111d9
uik6 <53418637+uik6@users.noreply.github.com>
2024-11-07 09:34:08 +0800

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,24 +1,28 @@
 import { useState } from "react";
 import { Button } from "antd";
+import { Greet } from "../../wailsjs/go/app/App";
 
 function Home() {
   const [count, setCount] = useState(0);
 
+  async function btn1(count: number) {
+    let result = await Greet(count);
+    setCount(result);
+  }
+
   return (
-    <>
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <h1>Vite + React</h1>
-        <div className="card" style={{placeItems: "center"}}>
-          <Button type="primary" size="large" onClick={() => setCount(count + 1)}>
-            count is {count}
-          </Button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <h1>Vite + React</h1>
+      <div className="card" style={{ placeItems: "center" }}>
+        <Button type="primary" size="large" onClick={() => btn1(count)}>
+          count is {count}
+        </Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
       </div>
-    </>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </div>
   );
 }
 
